2f4a937fc64ea3a8ece50fc0c66cc61c2aff8a03
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-20 00:02:17 +0900

    implement randam hand

--- SSP/ViewController.swift
+++ SSP/ViewController.swift
@@ -9,16 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var resultImageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    private var resultHand: Hand? = .Paper
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBOutlet weak var resultImageView: UIImageView!
-    @IBOutlet weak var resultLabel: UILabel!
+    
     @IBAction func shuffleAction(_ sender: Any) {
-        resultLabel.text = "グー"
-        resultImageView.image = UIImage(named: "Stone")
+        var newHand: Hand
+        repeat {
+            newHand = Hand.getSample()
+        } while newHand == resultHand
+        resultHand = newHand
+        
+        switch resultHand {
+        case .Stone:
+            resultLabel.text = "グー"
+            resultImageView.image = UIImage(named: "Stone")
+        case .Scissors:
+            resultLabel.text = "チョキ"
+            resultImageView.image = UIImage(named: "Scissors")
+        case .Paper:
+            resultLabel.text = "パー"
+            resultImageView.image = UIImage(named: "Paper")
+        default: break
+        }
+    }
+    
+    enum Hand: CaseIterable, Sampling {
+        case Stone
+        case Scissors
+        case Paper
     }
 }
 
+protocol Sampling {}
+extension Sampling where Self: CaseIterable {
+    static func getSample() -> Self {
+        let all = self.allCases as! [Self]
+        let index = Int.random(in: 0...all.count - 1)
+        return all[index]
+    }
+}
