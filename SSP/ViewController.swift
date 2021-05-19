c581fb540345a0c08cb235cc0bd09518658cd085
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-19 23:05:42 +0900

    bind storyboard and viewcontroller class

--- SSP/ViewController.swift
+++ SSP/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var resultImageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func shuffleAction(_ sender: Any) {
+    }
 }
 
